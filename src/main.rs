eedb684bcc3e769804ef11790ff886cd0fd9228f
Prathamesh247 <pphambar@gmail.com>
2022-01-18 01:44:18 +0530

    variables and data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+trait TypeDebug {
+    fn print_type(&self);
+}
+impl <T> TypeDebug for T {
+    fn print_type(&self) {
+        println!("The type of variable is '{}'", std::any::type_name::<T>());
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut x = "Hello";
+    println!("{}, world!", x);
+    x = "Bye";
+    println!("{}, world!", x);
+    x.print_type();
+
+    let y:i32 = 5;
+    y.print_type();
+
+    let t = (1, 2, 3);
+    let (one, two, _) = t;
+    println!("{} & {}", one, two);
+    t.print_type();
+
+    let a = [1; 3];
+    println!("The array is {:?}", a);
+    a.print_type();
 }
