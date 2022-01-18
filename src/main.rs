de8c1922c8146b14f57d4494f0bace807d3178b0
Prathamesh247 <pphambar@gmail.com>
2022-01-18 22:17:42 +0530

    Loops and few comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//print_type function lets us fetch the data type of a variable.
+
 trait TypeDebug {
     fn print_type(&self);
 }
@@ -8,7 +10,8 @@ impl <T> TypeDebug for T {
 }
 
 fn main() {
-
+    //Since variables in Rust are by default immutable,
+    //We make them mutable by adding the keyword "mut".
     let mut x = "Hello";
     println!("{}, world!", x);
     x = "Bye";
@@ -18,22 +21,59 @@ fn main() {
     let y:i32 = 5;
     y.print_type();
 
+    //Tuple
     let t = (1, 2, 3);
     let (one, two, _) = t;
     println!("{} & {}", one, two);
     t.print_type();
 
+    //Array
     let a = [1; 3];
     println!("The array is {:?}", a);
     a.print_type();
 
+    //if else if flow control
     let b = 7;
-
     if b%2 == 0 {
         println!("Number is Even");
     }  else if b%2 == 1 {
         println!("Number is Odd");
     }  else {
         println!("It's not an integer!")
+    }
+
+    //loops
+    let mut d = 2;
+    loop {
+        d = d*2;
+
+        if d>5000 {
+            break;
+        } else {
+            println!("d = {}", d);
+        }
+    }
+    println!("Loop Ends!");
+
+    //while loop
+    let mut w = 2;
+    while w < 5000 {
+        w = w*2;
+        println!("w = {}", w);
+    }
+
+
+    //implicit for loop
+    for p in 0..5 { //upper limit is exclusive
+        println!("p = {}", p*2);
+    }
+    //explicit for loop
+    for q in 0..=5 { //upper limit is inclusive
+        println!("q = {}", q*2);
+    }
+    //for loop used with iterators
+    let arr = [7, 8, 9]; 
+    for val in arr {
+        println!("The value is {}", val);
     }
 }
